1713568ed8f03aa0fa8b5df27cefe8be9fedf751
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2025-08-14 09:02:52 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -25,16 +25,16 @@ Repo Link : https://github.com/DigitalBotz/Digital-Rename-Bot
 License Link : https://github.com/DigitalBotz/Digital-Rename-Bot/blob/main/LICENSE
 """
 
-__name__ = "Digital-Rename-Bot"
+__name__ = "Anity/Light-Rename-Bot"
 __version__ = "3.0.8"
 __license__ = " Apache License, Version 2.0"
-__copyright__ = "Copyright (C) 2022-present Digital Botz <https://github.com/DigitalBotz>"
-__programer__ = "<a href=https://github.com/DigitalBotz/Digital-Rename-Bot>Digital Botz</a>"
+__copyright__ = "Copyright (C) 2022-present Digital Botz <https://github.com/Anitu_managementbots>"
+__programer__ = "<a href=https://github.com/Anity_managementbots/light_Rename_Bot>Anity/Light Botz</a>"
 __library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
 __language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
 __database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
-__developer__ = "<a href=https://t.me/Digital_Botz>Digital Botz</a>"
-__maindeveloper__ = "<a href=https://t.me/RknDeveloper>RknDeveloper</a>"
+__developer__ = "<a href=https://t.me/Shiro_OniGun>ShiroOni</a>"
+__maindeveloper__ = "<a href=https://t.me/beesmileee>Smile</a>"
 
 # main copyright herders (©️)
 # I have been working on this repo since 2022
